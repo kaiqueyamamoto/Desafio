d09be71eed53c153d45cf39a327cebae87ac7007
kaiqueyamamoto <kaique@redblock.com.br>
2025-12-15 03:17:05 -0300

    fix: adiciona polyfill para Request/Response no ambiente de testes

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,10 @@
 // Learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Polyfill para NextRequest no ambiente de teste
+if (typeof globalThis.Request === 'undefined') {
+  const { Request, Response, Headers } = require('undici');
+  globalThis.Request = Request;
+  globalThis.Response = Response;
+  globalThis.Headers = Headers;
+}
