be2a95ead1774a28d0a89505d8b17c9628081508
kaiqueyamamoto <kaique@redblock.com.br>
2025-12-15 02:46:58 -0300

    feat: define tema corporate do DaisyUI como padrão no layout

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="pt-BR" data-theme="light">
+    <html lang="pt-BR" data-theme="corporate">
       <body>{children}</body>
     </html>
   );
