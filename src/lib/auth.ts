6f85d7935761d8145d47fec9bcdbcad71a138d10
kaiqueyamamoto <kaique@redblock.com.br>
2025-12-15 03:17:17 -0300

    fix: corrige import do jsonwebtoken para compatibilidade

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,4 @@
-import jwt from 'jsonwebtoken';
+import * as jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import { prisma } from './db';
 import { JwtPayload, RegisterDto, LoginDto } from '@/types';
